9cb33c2dadf1fe34f4438ebf784aa455d6ddb46e
Mat McCully <mccully1894@gmail.com>
2016-09-08 19:15:59 -0500

    almost ded

--- server/public/scripts/controllers/homeController.js
+++ server/public/scripts/controllers/homeController.js
@@ -21,24 +21,35 @@ myApp.controller('homeController', ['$scope', '$http', '$location', function($sc
   var key = '50314a4cdf0488cfd23dcb086e0b3cb7';
   var baseURL = 'food2fork.com/api/';
   var conversonURL = "https://json2jsonp.com/?url="
-  // var recId = 35382;
   $scope.getRecipes = function() {
-    var query = conversonURL + baseURL + 'get';
+    var query =  conversonURL + baseURL + 'search';
     query += '?key=' + key;
-    query += '&rId=29159';
-    var request = encodeURI(query);
     var request = encodeURI(query) + '&callback=JSON_CALLBACK';
 
     $http.jsonp(request).then(function(response) {
       $scope.results = response.data;
       console.log(request);
-      console.log(response.data);
+      console.log($scope.results.recipes[0].title);
+
+      $scope.getAllrecipes = function() {
+        for(var i = 0; i < $scope.results.recipes.length;i++){
+          console.log($scope.results.recipes.length);
+          $scope.allRecipes = $scope.results.recipes[0].title;
+          console.log($scope.results.recipes[i]);
+          console.log("recipe titles: " , $scope.allRecipe);
+        }
+      }
     });
-
-
   };
 
-  angular.element(document).ready($scope.getRecipes);
+  // $scope.getAllrecipes = function() {
+  //   getRecipes();
+  //   for(var i = 0; i <= $scope.results.recipes.length;i++){
+  //     $scope.allRecipes = $scope.results.recipes[i].title;
+  //     console.log("recipe titles: " , $scope.allRecipe);
+  //   }
+  // }
+  // angular.element(document).ready($scope.getRecipes);
 
 
 }]);
